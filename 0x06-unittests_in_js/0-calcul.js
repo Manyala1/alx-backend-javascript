74328ecffd6fa1df85405ee40a102e5d4f12fc79
manyala1 <paulmuthama60@gmail.com>
2024-11-28 19:49:06 +0000

    add

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,9 @@
+module.exports = function calculateNumber(a, b) {
+	const numA = Number(a);
+	const numB = Number(b);
+
+	if (number.isNaN(numA) || Number.isNaN(numB))
+	throw TypeError;
+
+	return (Math.round(numA) + Matth.round(numB));
+}
